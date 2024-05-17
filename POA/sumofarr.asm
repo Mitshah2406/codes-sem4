05bb4e26be223a8f4a07afc994bacc782e125f65
Aditya6273 <adityashah2701@gmail.com>
2024-05-17 15:42:29 +0530

    Update sumofarr.asm

--- POA/sumofarr.asm
+++ POA/sumofarr.asm
@@ -1,41 +1,44 @@
 DATA SEGMENT
-    ARR DB 1H,2H,3H,4H,5H,6H,7H,8H,9H
-    LEN DW $-ARR
-    SUM DW ?  
+    ARR DB 01H,02H,03H,04H,05H  
+     LEN DW $-ARR
+    SUM DW ?
+   
     AVG DW ?  
 DATA ENDS
 
 CODE SEGMENT
-    ASSUME CS:CODE,DS:DATA
-START:
-    MOV AX,DATA
-    MOV DS,AX
-   
-    MOV CX,LEN
-    LEA SI,ARR
-   
-    MOV AX,0000H  
-    MOV BX,0000H  
-   
-SUM_LOOP:    
-    MOV BL,[SI]
-    ADD AX,BX    
+    ASSUME DS:DATA, DS:DATA
+   START:
+    
+    MOV AX, DATA
+    MOV DS, AX
+    
+    MOV CX, LEN
+    lea SI,ARR
+    
+    MOV AX, 0000H
+    MOV BX, 0000H
+    
+LOOP_SUM:
+    
+    MOV BL, [SI]
+    ADD AX,BX
     INC SI
     DEC CX
-    JNZ SUM_LOOP  
-   
-    MOV SUM,AX    
-   
-    MOV BX,0      
-    MOV BX,LEN    
-   
-    MOV AX,SUM    
-    DIV BL        
-   
-    MOV AVG,AX    
-   
-    MOV AH,4CH
+    JNZ LOOP_SUM
+    
+    MOV SUM , AX
+    
+    MOV BX, 0
+    MOV Bx ,LEN
+    MOV AX,SUM
+    DIV BL    
+    MOV AVG, AX
+    
+    
+    MOV AH, 4CH
     INT 21H
-   
-    CODE ENDS
+    
+ CODE ENDS
 END START
+    
