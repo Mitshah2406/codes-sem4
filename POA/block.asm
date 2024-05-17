9741d31cd602f247bec136a5d87dafa0bb226d4d
Aditya6273 <adityashah2701@gmail.com>
2024-05-17 12:42:16 +0530

    Update block.asm

--- POA/block.asm
+++ POA/block.asm
@@ -1,47 +1,30 @@
-
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
-             
-             
-
-           
 DATA SEGMENT
-    N1 DB 45H,32H,36H
-DATA ENDS    
+N1 DB 4H,5H,6H,7H,8H
+DATA ENDS
 
 EXTRA SEGMENT
-    N2 DB ?
+N2 DB ?
 EXTRA ENDS
 
+CODE SEGMENT  
+START:
+MOV AX, DATA
+MOV DS, AX
 
-CODE SEGMENT
-    START:
-    MOV AX,DATA
-    MOV DS,AX
-    
-    MOV AX,EXTRA
-    MOV ES,AX
-    
-    LEA SI,N1
-    LEA DI,N2    
-    
-    MOV CL,04H
-    
-    L1:
-    MOV AX,DS:[SI]
-    MOV ES:[DI],AX
-    
-    INC SI
-    INC DI
-    DEC CL
-    
-    JNZ L1
+MOV AX, EXTRA
+MOV ES, AX
+
+MOV CL, 06H
+LEA SI, N1
+LEA DI, N2
+
+L1:
+   MOV AX,DS:[SI]
+   MOV ES:[DI],AX
+   
+   INC SI
+   INC DI
+   DEC CL
+   JNZ L1
 ENDS
 END START
-
-
-
-ret
-
-
-
